ee5e51dd9b2be4135d41ef48de632b36ef6779bd
sifanduronto <sifan@sifans-MacBook-Air.local>
2021-01-15 03:03:38 +0600

    group user population update

--- server/src/hooks/collectives.js
+++ server/src/hooks/collectives.js
@@ -2,13 +2,13 @@ const { BadRequest } = require('@feathersjs/errors');
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     return async context => {
 
-        const { data, method, result, params,authentication } = context;
+        const { data, method, result, params, authentication } = context;
         data.ownerId = params.user._id
-        data.users = [params.user._id]
+        data.users = [{ _id: params.user._id, avatar: params.username.avatar, username: params.user.avatar }]
         if (data.collectiveName.length < 4) {
             throw new BadRequest('Invalid name', data, { errors: { name: 'Name should be greater than 4 character' } });
         }
-        
+
         return context;
     };
 };

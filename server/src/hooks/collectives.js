8bb0a2348c38d24bf7b8de8f002f38268db4c664
sifanduronto <sifan@sifans-MacBook-Air.local>
2021-01-16 21:41:26 +0600

    skills and tags service added

--- server/src/hooks/collectives.js
+++ server/src/hooks/collectives.js
@@ -2,9 +2,9 @@ const { BadRequest } = require('@feathersjs/errors');
 module.exports = function (options = {}) { // eslint-disable-line no-unused-vars
     return async context => {
 
-        const { data, method, result, params, authentication } = context;
+        const { data, method, result, params } = context;
         data.ownerId = params.user._id
-        data.users = [{ _id: params.user._id, avatar: params.username.avatar, username: params.user.avatar }]
+        data.users = [params.user]
         if (data.collectiveName.length < 4) {
             throw new BadRequest('Invalid name', data, { errors: { name: 'Name should be greater than 4 character' } });
         }
